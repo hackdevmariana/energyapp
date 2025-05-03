a7fd0f0a0092a39198ade7755802e4361095c098
fernando <fernando@juandemariana.org>
2025-05-03 23:54:20 +0200

    Add src/theme.js to project

---
+++ src/theme.js
@@ -0,0 +1,53 @@
+export const lightTheme = {
+  ...MD3LightTheme, // Extiende el tema predeterminado de Paper
+  colors: {
+    ...MD3LightTheme.colors,
+    background: "#fafafa",
+    text: "#0c0918",
+    primary: "#463beb",
+    secondary: "#eda71a",
+    income: "#128c5b",
+    expenses: "#db2c2c",
+    nowhite: "#fafafa",
+    noblack: "#0c0918",
+  },
+  typography: {
+    general: {
+      fontFamily: "Urbanist",
+      fontSize: 16,
+      fontWeight: "400",
+    },
+    featured: {
+      fontFamily: "Anton",
+      fontSize: 20,
+      fontWeight: "700",
+    },
+  },
+};
+
+export const darkTheme = {
+  ...MD3DarkTheme,
+  colors: {
+    ...MD3DarkTheme.colors,
+    background: "#0c0918",
+    text: "#fafafa",
+    primary: "#463beb",
+    secondary: "#eda71a",
+    income: "#128c5b",
+    expenses: "#db2c2c",
+    nowhite: "#fafafa",
+    noblack: "#0c0918",
+  },
+  typography: {
+    general: {
+      fontFamily: "Urbanist",
+      fontSize: 16,
+      fontWeight: "400",
+    },
+    featured: {
+      fontFamily: "Anton",
+      fontSize: 20,
+      fontWeight: "700",
+    },
+  },
+};
